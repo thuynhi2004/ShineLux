50c76feda6181c1084762087a26cde20387dc801
nhi <tnhi26781@gmail.com>
2025-04-27 15:59:07 +0700

    add

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 async function connect() {
-    try {
-        const databaseUrl = 'mongodb+srv://ptthuynhcntt2211017:WdS95PxlQnbvrFlj@doan01.nstcg.mongodb.net/DoAn02-database?retryWrites=true&w=majority&appName=DoAn01';
-        await mongoose.connect(databaseUrl);
-        console.log('Kết nối database thành công!');
-    } catch (error) {
-        console.log(`Lỗi: ${error}`);
-    }
+  try {
+    const databaseUrl =
+      "mmongodb+srv://ThuyNhi:<db_password>@cluster0.hnlfqgb.mongodb.net/ShineLux?retryWrites=true&w=majority&appName=Cluster0";
+    await mongoose.connect(databaseUrl);
+    console.log("Kết nối database thành công!");
+  } catch (error) {
+    console.log(`Lỗi: ${error}`);
+  }
 }
-
 
 module.exports = { connect };
