ae2e8b448c0aca0b8a195e8f39e89910358c56a0
nhi <tnhi26781@gmail.com>
2025-04-29 20:13:45 +0700

    add dbconfig.js

--- src/config/dbconfig.js
+++ src/config/dbconfig.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 async function connect() {
   try {
     const databaseUrl =
-      "mmongodb+srv://ThuyNhi:<db_password>@cluster0.hnlfqgb.mongodb.net/ShineLux?retryWrites=true&w=majority&appName=Cluster0";
+      "mongodb+srv://ThuyNhi:1234@cluster0.hnlfqgb.mongodb.net/ShineLux?retryWrites=true&w=majority&appName=Cluster0";
     await mongoose.connect(databaseUrl);
     console.log("Kết nối database thành công!");
   } catch (error) {
